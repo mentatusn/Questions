756eedad79cd16883f01198d8267e0083ee71c45
Andej Nesterenko <mentatus.n@gmail.com>
2023-05-07 13:52:06 +0300

    4 onBackPressed

--- app/src/main/java/com/example/questions/MainActivity.kt
+++ app/src/main/java/com/example/questions/MainActivity.kt
@@ -3,13 +3,36 @@ package com.example.questions
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContentProviderCompat.requireContext
 
 class MainActivity : AppCompatActivity() {
+
+    private fun showExitConfirmationDialog() {
+        AlertDialog.Builder(this)
+            .setMessage("Вы уверены, что хотите выйти?")
+            .setPositiveButton("Да") { _, _ ->
+                onBackPressedCallback.isEnabled = false
+                finish()
+            }
+            .setNegativeButton("Нет", null)
+            .show()
+    }
+
+    val onBackPressedCallback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            showExitConfirmationDialog()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.d("testLife", "onCreate Activity")
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        onBackPressedDispatcher.addCallback(this, onBackPressedCallback)
 
         supportFragmentManager
             .beginTransaction()
